87cbb4fd7b60cbf18a526a29352e85679f054f3c
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2023-03-14 20:20:53 -0300

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { Container } from "./styles"
+import { Button, Container, Input } from "./styles"
+import styles from "./App.module.css"
 
 export default function App() {
   return (
     <Container>
-      <h1 className="title">To Do List</h1>
+      <h1 className={styles.title}>To Do List</h1>
+    <div className={styles.container}>
+      <Input placeholder="Digite sua tarefa" />
+      <Button>Adicionar</Button>
+      </div>
     </Container>
   )
 }
--- src/styles/index.js
+++ src/styles/index.js
@@ -21,14 +21,48 @@ export const Container = styled.div`
     flex-direction: column;
     align-itens: center;
     padding: 12px 25px
+`
 
-.title {
-    font-style: normal;
-    font-weight: 600;
-    font-size: 48px;
-    line-height: 72px
-    color: #da2535;
+export const Input = styled.input`
+padding: 8px 24px;
+width: 224px;
+height: 50;
+background: #FFFFFF;
+box-shadow: 0px 4px 4px rgba(0,0,0,0.25);
+border-radius: 10px;
+border: none;
 
-    mix-blend-mode: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 24px;
+color: #da2535;
+
+&::placeholder{
+    font-weight: 700;
+font-size: 16px;
+line-height: 24px;
+color: #da2535;
 }
+`
+
+// export const Flex = styled.div`
+// display: flex;
+// flex-direction: column;
+// justify-content: center;
+// align-items: center;
+// `
+
+export const Button = styled.button`
+width: 112px;
+height: 50px;
+background: #da2535;
+border-radius: 10px;
+border: none;
+margin-left: 10px;
+
+font-weight: 500;
+font-size: 16px;
+line-height: 24px;
+color: #fbfbfb;
+
 `
